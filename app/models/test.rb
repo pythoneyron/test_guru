d4191255bf465c334d54c4e759c63e53cacd6a70
Олег Платонов <pythoneyron@gmail.com>
2021-05-26 15:44:30 +0500

    fix test.rb added foreign key

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
 
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: author_id
 
   def self.by_category_name(category)
     joins(:category).where(categories: { title: category }).order(id: :desc).pluck('tests.title')
