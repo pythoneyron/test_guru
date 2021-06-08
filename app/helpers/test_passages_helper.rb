934fc6981e134c8f5585aed4cad3bc3efdf31254
Олег Платонов <pythoneyron@gmail.com>
2021-06-08 16:21:46 +0500

    added display result with percent

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,7 @@
 module TestPassagesHelper
+  def percentage_result(test_passage)
+    count_questions = test_passage.test.questions.count
+    count_current_answers = test_passage.correct_questions
+    100.0 / count_questions * count_current_answers
+  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,6 +4,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :next_question
 
   def completed?
     current_question.nil?
@@ -14,8 +15,11 @@ class TestPassage < ApplicationRecord
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
+  end
+
+  def count_questions
+    self.test.questions.where("id <= ?", current_question.id).count
   end
 
   private
@@ -25,10 +29,11 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-    correct_answers_count == answer_ids.count
+    # correct_answers_count = correct_answers.count
+    #
+    # (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
+    # correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort if answer_ids
   end
 
   def correct_answers
@@ -36,6 +41,6 @@ class TestPassage < ApplicationRecord
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
