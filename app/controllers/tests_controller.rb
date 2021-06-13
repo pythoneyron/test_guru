983abacf001e76bb775aec7d51e9c0f3daa49286
Платонов Олег <pythoneyron@gmail.com>
2021-06-14 00:55:28 +0500

    remove methon find_user

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,6 @@ class TestsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :find_test, only: %i[show destroy edit update start]
-  before_action :find_user, only: %i[start]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -46,10 +45,6 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def find_user
-    @user = current_user
   end
 
   def test_params
