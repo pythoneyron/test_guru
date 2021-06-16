2b92e0847eb1dd35c0123d64293d9784d99f0c0a
Олег Платонов <pythoneyron@gmail.com>
2021-06-16 08:34:27 +0500

    fix code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
          :registerable,
          :recoverable,
          :rememberable,
-         :validatable
+         # :trackable,
+         :validatable,
+         :confirmable
 
   has_many :test_passages
   has_many :tests, through: :test_passages
