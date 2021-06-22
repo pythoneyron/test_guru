29b2665608c9dd5b2d22993b4589cd60873fae18
Платонов Олег <pythoneyron@gmail.com>
2021-06-22 07:06:40 +0500

    install bootstrap

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ gem 'devise', '~> 4.0'
 
 gem 'rails-i18n', '~> 6.0.0'
 
+gem 'jquery-rails'
+gem 'bootstrap', '~> 5.0.0'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap-daterangepicker/daterangepicker'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
