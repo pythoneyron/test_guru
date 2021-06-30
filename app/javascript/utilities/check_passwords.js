9338800c7f3e7b80e71a24be8e1796a6916cc76a
Олег Платонов <pythoneyron@gmail.com>
2021-06-30 10:40:01 +0500

    fix code

--- app/javascript/utilities/check_passwords.js
+++ app/javascript/utilities/check_passwords.js
@@ -1,23 +1,25 @@
 document.addEventListener('turbolinks:load', function () {
-    let passwordInput = document.querySelectorAll('.password')
+    let passwordInputAll = document.querySelectorAll('.password')
 
-    document.querySelectorAll('.password').forEach(item => {
+    passwordInputAll.forEach(item => {
         item.addEventListener('input', event => {
             let password = document.querySelector('#user_password').value
             let password_confirmation = document.querySelector('#user_password_confirmation').value
 
-            if (password != password_confirmation) {
-                passwordInput.forEach(item => {
-                    item.classList.add('invalid')
-                    item.classList.remove('valid')
-                })
-            } else if (password.length == 0 && password_confirmation.length == 0) {
-                passwordInput.forEach(item => {
+            if (password.length == 0 && password_confirmation.length == 0) {
+                return passwordInputAll.forEach(item => {
                     item.classList.remove('valid')
                     item.classList.remove('invalid')
                 })
+            }
+
+            if (password != password_confirmation) {
+                return passwordInputAll.forEach(item => {
+                    item.classList.add('invalid')
+                    item.classList.remove('valid')
+                })
             } else {
-                passwordInput.forEach(item => {
+                passwordInputAll.forEach(item => {
                     item.classList.add('valid')
                     item.classList.remove('invalid')
                 })
