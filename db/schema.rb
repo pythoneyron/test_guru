82e553e27c8de8d69bf966fd2c69a5333b212e88
Олег Платонов <pythoneyron@gmail.com>
2021-07-06 16:26:51 +0500

    added action mailer

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_06_24_173713) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
     t.boolean "correct", default: false, null: false
