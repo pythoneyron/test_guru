f1ef8ee614b7a4f02f44ce055d48d28260809bc1
Олег Платонов <pythoneyron@gmail.com>
2021-07-15 15:49:02 +0500

    fix code

--- app/services/test_passage_service.rb
+++ app/services/test_passage_service.rb
@@ -21,7 +21,7 @@ class TestPassageService
 
     tests_backend_user = @user.tests.success_tests.distinct.count
 
-    true if tests_backend == tests_backend_user && !@user.badges.include?(badge)
+    tests_backend == tests_backend_user && !@user.badges.include?(badge)
   end
 
   def success_on_first_try_award?(badge)
@@ -30,13 +30,13 @@ class TestPassageService
 
     tests_passages = TestPassage.where(user_id: user.id, test_id: test.id)
 
-    true if tests_passages.count == 1
+    tests_passages.count == 1
   end
 
   def success_certain_level_award?(badge)
     tests_by_level = Test.where(level: TEST_LEVEL).count
     correct_tests_by_level = @user.tests.success_tests.where(level: TEST_LEVEL).distinct.count
 
-    true if tests_by_level == correct_tests_by_level
+    tests_by_level == correct_tests_by_level
   end
 end
